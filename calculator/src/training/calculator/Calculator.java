ffb3ef37f623e2baef56dd07420cf297c2d2620a
Dan Rosen <dr@mergeconflict.com>
2013-03-08 10:13:28 -0800

    refactor a bit, fix ordering bug

--- calculator/src/training/calculator/Calculator.java
+++ calculator/src/training/calculator/Calculator.java
@@ -4,18 +4,34 @@ import java.util.Stack;
 
 public class Calculator {
 
-	/*
-	 * 1. tokenize the string
-	 * 2. for each token
-	 *    - if it's a number, push it on stack
-	 *    - if it's an operator, pop two elements from stack
-	 *      perform operation (remember order of lhs and rhs!)
-	 *      push result
-	 *    - if it's neither: throw
-	 * 3. pop last element from stack
-	 * 
-	 * "1 2 3 + - 4 5 * 6 / +"  ==>  -1
-	 */
+	public static boolean handleNumber(String token, Stack<Integer> stack) {
+		try {
+			stack.push(Integer.parseInt(token));
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+
+	public static boolean handleOperator(String token, Stack<Integer> stack) {
+		if (token.equals("+")) {
+			int rhs = stack.pop(), lhs = stack.pop();
+			stack.push(lhs + rhs);
+		} else if (token.equals("-")) {
+			int rhs = stack.pop(), lhs = stack.pop();
+			stack.push(lhs - rhs);
+		} else if (token.equals("*")) {
+			int rhs = stack.pop(), lhs = stack.pop();
+			stack.push(lhs * rhs);
+		} else if (token.equals("/")) {
+			int rhs = stack.pop(), lhs = stack.pop();
+			stack.push(lhs / rhs);
+		} else {
+			return false;
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
 		if (args.length != 1) {
 			System.err.println("usage: Calculator <expression>");
@@ -24,21 +40,8 @@ public class Calculator {
 
 		Stack<Integer> stack = new Stack<Integer>();
 		for (String token : args[0].split(" ")) {
-			try {
-				stack.push(Integer.parseInt(token));
-			} catch (NumberFormatException e) {
-				int rhs = stack.pop(), lhs = stack.pop();
-				if (token.equals("+")) {
-					stack.push(lhs + rhs);
-				} else if (token.equals("-")) {
-					stack.push(lhs - rhs);
-				} else if (token.equals("*")) {
-					stack.push(lhs * rhs);
-				} else if (token.equals("/")) {
-					stack.push(lhs / rhs);
-				} else {
-					throw new IllegalArgumentException("invalid token: " + token);
-				}
+			if (!handleOperator(token, stack) && !handleNumber(token, stack)) {
+				throw new IllegalArgumentException("invalid token: " + token);
 			}
 		}
 		System.out.println(stack.pop());
